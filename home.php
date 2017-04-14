b223d82c822685072af39e64561c62e3adcfa85e
João Valente <filipevalente@gmail.com>
2017-04-14 14:40:32 +0100

    wraps the blog related pages in a content div for top padding.

--- home.php
+++ home.php
@@ -3,38 +3,38 @@
 Template Name: Blog Index
 */
 get_header(); ?>
+<div class='content'>
+    <div class='blog'>
+        <div class='row'>
+            <div class='small-12 medium-5 medium-centered columns'>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); $counter++ ?>
+                    <a class='go_to_post' href='<?php echo get_post_permalink(); ?>'>
+                        <?php if($counter == 1) : ?>
+                            <div class='row'>
+                                <div class='small-12 columns first_post'>
+                                    <?php the_post_thumbnail(); ?>
+                                    <time><?php the_date(); ?></time>
+                                    <h4><?php the_title(); ?></h4>
+                                    <p class='excerpt'><?php echo get_the_excerpt(); ?></p>
+                                </div>
+                            </div>
+                        <?php else : ?>
+                            <div class='row'>
+                                <div class='small-4 columns'>
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
 
-<div class='blog'>
-    <div class='row'>
-        <div class='small-12 medium-5 medium-centered columns'>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); $counter++ ?>
-                <a class='go_to_post' href='<?php echo get_post_permalink(); ?>'>
-                    <?php if($counter == 1) : ?>
-                        <div class='row'>
-                            <div class='small-12 columns first_post'>
-                                <?php the_post_thumbnail(); ?>
-                                <time><?php the_date(); ?></time>
-                                <h4><?php the_title(); ?></h4>
-                                <p class='excerpt'><?php echo get_the_excerpt(); ?></p>
+                                <div class='small-8 columns'>
+                                    <time><?php the_date(); ?></time>
+                                    <h5><?php the_title(); ?></h5>
+                                </div>
                             </div>
-                        </div>
-                    <?php else : ?>
-                        <div class='row'>
-                            <div class='small-4 columns'>
-                                <?php the_post_thumbnail(); ?>
-                            </div>
-
-                            <div class='small-8 columns'>
-                                <time><?php the_date(); ?></time>
-                                <h5><?php the_title(); ?></h5>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                </a>
-            <?php endwhile; else : ?>
-            <?php endif; ?>
+                        <?php endif; ?>
+                    </a>
+                <?php endwhile; else : ?>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </div>
-
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,50 +1,50 @@
 <?php get_header(); ?>
-
-<div class='single_post'>
-    <div class='row'>
-        <div class='small-12 medium-7 medium-centered columns'>
-            <?php while (have_posts()) : the_post(); ?>
-                <h3 class='title'><?php the_title(); ?></h3>
-                <div class='content'><?php the_content(); ?></div>
-                <div class='row'>
-                    <div class='previous small-12 medium-6 columns'>
-                        <?php
-                        $prev_post = get_previous_post();
-                        if (!empty( $prev_post )): ?>
-                            <a href="<?php echo get_permalink( $prev_post->ID ); ?>">
-                                <div class='row'>
-                                    <div class='small-4 columns'>
-                                        <?php echo get_the_post_thumbnail($prev_post, 'thumbnail') ?>
+<div class='content'>
+    <div class='single_post'>
+        <div class='row'>
+            <div class='small-12 medium-7 medium-centered columns'>
+                <?php while (have_posts()) : the_post(); ?>
+                    <h3 class='title'><?php the_title(); ?></h3>
+                    <div class='content'><?php the_content(); ?></div>
+                    <div class='row'>
+                        <div class='previous small-12 medium-6 columns'>
+                            <?php
+                            $prev_post = get_previous_post();
+                            if (!empty( $prev_post )): ?>
+                                <a href="<?php echo get_permalink( $prev_post->ID ); ?>">
+                                    <div class='row'>
+                                        <div class='small-4 columns'>
+                                            <?php echo get_the_post_thumbnail($prev_post, 'thumbnail') ?>
+                                        </div>
+                                        <div class='small-8 columns'>
+                                            <p>Previous</p>
+                                            <h6><?php echo $prev_post->post_title; ?></h6>
+                                        </div>
                                     </div>
+                                </a>
+                            <?php endif ?>
+                        </div>
+                        <div class='small-12 medium-6 columns next'>
+                            <?php
+                            $next_post = get_next_post();
+                            if (!empty( $next_post )): ?>
+                            <a href="<?php echo get_permalink( $next_post->ID ); ?>">
+                                <div class='row'>
                                     <div class='small-8 columns'>
-                                        <p>Previous</p>
-                                        <h6><?php echo $prev_post->post_title; ?></h6>
+                                        <p>Next</p>
+                                        <h6><?php echo $next_post->post_title; ?></h6>
+                                    </div>
+                                    <div class='small-4 columns'>
+                                        <?php echo get_the_post_thumbnail($next_post, 'thumbnail') ?>
                                     </div>
                                 </div>
                             </a>
-                        <?php endif ?>
+                        <?php endif; ?>
                     </div>
-                    <div class='small-12 medium-6 columns next'>
-                        <?php
-                        $next_post = get_next_post();
-                        if (!empty( $next_post )): ?>
-                        <a href="<?php echo get_permalink( $next_post->ID ); ?>">
-                            <div class='row'>
-                                <div class='small-8 columns'>
-                                    <p>Next</p>
-                                    <h6><?php echo $next_post->post_title; ?></h6>
-                                </div>
-                                <div class='small-4 columns'>
-                                    <?php echo get_the_post_thumbnail($next_post, 'thumbnail') ?>
-                                </div>
-                            </div>
-                        </a>
-                    <?php endif; ?>
-                </div>
-                </div>
-            <?php endwhile; ?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
         </div>
     </div>
 </div>
-
 <?php get_footer(); ?>
